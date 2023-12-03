8333de1ad678150ef5411fc820b6e10119f83402
estney <esther.pxy@gmail.com>
2023-12-03 12:50:01 +0800

    saved package files.

--- src/TimerSettings.jsx
+++ src/TimerSettings.jsx
@@ -10,35 +10,32 @@ const TimerSettings = ({
 }) => {
   return (
     <>
-      <button onClick={() => setShowSetting(true)}>Settings</button>
-      {showSetting && (
+      <div>
+        <h2>Settings</h2>
         <div>
-          <h2>Settings</h2>
-          <div>
-            <label>
-              Work Duration (minutes):
-              <input
-                type="number"
-                min="1"
-                value={workDuration / 60}
-                onChange={handleWorkDurationChange}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Break Duration (minutes):
-              <input
-                type="number"
-                min="1"
-                value={breakDuration / 60}
-                onChange={handleBreakDurationChange}
-              />
-            </label>
-          </div>
-          <button onClick={() => setShowSetting(false)}>Close Settings</button>
+          <label>
+            Work Duration (minutes):
+            <input
+              type="number"
+              min="1"
+              value={workDuration / 60}
+              onChange={handleWorkDurationChange}
+            />
+          </label>
         </div>
-      )}
+        <div>
+          <label>
+            Break Duration (minutes):
+            <input
+              type="number"
+              min="1"
+              value={breakDuration / 60}
+              onChange={handleBreakDurationChange}
+            />
+          </label>
+        </div>
+        <button onClick={() => setShowSetting(false)}>Close Settings</button>
+      </div>
     </>
   );
 };
