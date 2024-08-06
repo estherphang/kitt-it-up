005cf1879db02576708c96fc9ed8ad42fcfa4b0f
estney <esther.pxy@gmail.com>
2024-08-06 08:54:24 +0800

    pwa completed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,50 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "PomoKitty",
+        short_name: "PomoKitty",
+        icons: [
+          {
+            src: "/android-chrome-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/android-chrome-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#01579b",
+        theme_color: "#01579b",
+        description: "A pomodoro timer with a cute kitty theme.",
+      },
+    }),
+  ],
   base: "/",
 });
